f5b45d444e0c4b2f124e2b1cdb06f183468894a0
marko <markolovic@hotmail.com>
2014-08-31 18:07:17 +0000

    corrected sp controller name

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'statics_pages/home'
+  get 'static_pages/home'
 
-  get 'statics_pages/help'
+  get 'static_pages/help'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
